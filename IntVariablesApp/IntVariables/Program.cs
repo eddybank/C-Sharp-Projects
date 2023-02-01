95ce875cad59f0aa6b9a0339e67919de066b42f6
eddybank <ehorbinski@gmail.com>
2023-02-01 01:07:44 -0500

    Int Variables

---
+++ IntVariablesApp/IntVariables/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// Yes - 2, 18, 2854, -9
+// No - 1.2, -5.7
+
+int age = 0;
+
+age = 43;
+
+int ageInTenYears = age + 10;
+
+// Range of integers is +/- 2.147B or 2^31
+// signed Int32
+// unsigned - 4.294 billion
+// bits - 0 or 1
+// byte - 8 bits - 00000000
+// 1, 11, 111, 1111
+
+Console.WriteLine(age);
+Console.WriteLine(ageInTenYears);
+
+// Don't divide ints - Console.WriteLine(age / 2);
+
