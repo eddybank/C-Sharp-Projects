4016434d2bdff09ed8187f72579ed362eee297eb
eddybank <ehorbinski@gmail.com>
2023-02-01 00:30:09 -0500

    2nd Console Project
    Greet user
    Ask for users first name
    Respond to user using their name

---
+++ MySecondProject/GreetingApp/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+// Welcome the user to the application
+Console.WriteLine("Welcome to the Greeting Application");
+Console.WriteLine("This application was built by Evan Horbinski");
+Console.WriteLine("--------------------------------------------");
+Console.WriteLine();
+
+// Ask the user for their first name
+string firstName;
+
+Console.Write("Please enter your First Name: ");
+firstName = Console.ReadLine();
+
+// Greet the user by their name
+Console.WriteLine();
+Console.WriteLine("Hello {0}!", firstName);
+Console.WriteLine("Thank you for using my application.");
+Console.ReadLine();
+
