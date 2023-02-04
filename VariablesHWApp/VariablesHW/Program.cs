3574afaf780def2b3d81bf04a86cd1f6295191df
eddybank <ehorbinski@gmail.com>
2023-02-04 00:10:53 -0500

    Variables HW

---
+++ VariablesHWApp/VariablesHW/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// Create a Console Application that has variables to hold a person's name, age, if they are alive, and their phone number. You do not need to capture these values from the user.
+// hold a person's name
+string name;
+
+// hold a person's age
+int age;
+
+// hold whether a person is alive
+bool alive;
+
+// hold a person's phone number
+string phoneNumber;
+
