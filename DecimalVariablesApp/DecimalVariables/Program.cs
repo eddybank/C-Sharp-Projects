66e514cacc5916499bcac2d7b2dba8a0b59536d0
eddybank <ehorbinski@gmail.com>
2023-02-03 23:50:26 -0500

    Decimal Variables

---
+++ DecimalVariablesApp/DecimalVariables/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// 4.12, 32, 1.234
+// 4.12, 32, 1.234
+
+// Int32 - 00000000 00000000 00000000 00000001
+
+decimal bankAccountBalance;
+
+// If you simply use 2.34, bankAccountBalance = 2.34;, compiler will see this as a double. 
+// Need to indicate its type using M
+bankAccountBalance = 2.34M;
+
+Console.WriteLine(bankAccountBalance);
