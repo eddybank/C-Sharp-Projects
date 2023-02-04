82aa99e16f3124f5488ce1f5af2a91e467437a55
eddybank <ehorbinski@gmail.com>
2023-02-04 01:30:29 -0500

    DateOnly Variable

---
+++ DateOnlyVariableApp/DateOnlyVariable/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+DateTime today = DateTime.Now;
+
+DateOnly birthday = DateOnly.Parse("6/11/1998");
+
+Console.WriteLine(birthday.ToString("MMMM dd yyyy"));
+
+Console.WriteLine($"Today full format: {today}");
+
+// DateTime.Date will produce date with time set to 12:00:00 (00:00:00) so formatting is necessary in situations that dont expect any time
+Console.WriteLine($"Today just date: {today.Date.ToString("MM/d/yyyy")}");
+
+Console.WriteLine($"Birthday full format: {birthday}");
