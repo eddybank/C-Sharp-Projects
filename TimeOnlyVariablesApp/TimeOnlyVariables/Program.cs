30367ec6b69aa6f8d4750a6bb1128bce54981030
eddybank <ehorbinski@gmail.com>
2023-02-04 01:33:26 -0500

    Time Only Variables

---
+++ TimeOnlyVariablesApp/TimeOnlyVariables/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+TimeOnly opensAt = TimeOnly.Parse("8:00 AM");
+
+// get time only from DateTime
+TimeOnly rightNow = TimeOnly.FromDateTime(DateTime.Now);
+
+Console.WriteLine(opensAt.ToString());
+Console.WriteLine(rightNow.ToString());
