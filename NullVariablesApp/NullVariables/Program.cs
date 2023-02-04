59e1ae557c55380522314c15d01e43108f1e0ff5
eddybank <ehorbinski@gmail.com>
2023-02-04 00:03:33 -0500

    Null Variable
    nullable - ?

---
+++ NullVariablesApp/NullVariables/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+// null - lack of value
+
+// haven't asked for the age yet
+int? age = null;
+
+// ? will tell compiler that variable is nullable, without it will throw warning/error
+bool? birthday = null;
+double? average = null;
+string? firtName = null;
+
+// ask for age here
+age = 0;
